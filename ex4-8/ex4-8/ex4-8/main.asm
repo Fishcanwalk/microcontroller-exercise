5ffe5a45a26dd1e8784fabad37d19d2cbb95090f
SoravitSukkarn <soravit.sukkarn@gmail.com>
2026-03-25 16:35:49 +0700

    fix: problem 4.8 incomplete problem

--- ex4-8/ex4-8/ex4-8/main.asm
+++ ex4-8/ex4-8/ex4-8/main.asm
@@ -13,14 +13,25 @@
 .org 0x0000
 	rjmp start
 
-TABLE:	.DB 0b0000, 0b0001
+BIN_TO_GRAY_TABLE:	
+		.DB 0b0000, 0b0001
 		.DB 0b0011, 0b0010
 		.DB 0b0110, 0b0111
 		.DB 0b0101, 0b0100
 		.DB 0b1100, 0b1101
-		.DB 0b1111, 0b1011
+		.DB 0b1111, 0b1110
 		.DB 0b1010, 0b1011
 		.DB 0b1001, 0b1000
+
+GRAY_TO_BIN_TABLE:
+		.DB 0b0000, 0b0001
+		.DB 0b0011, 0b0010
+		.DB 0b0111, 0b0110
+		.DB 0b0100, 0b0101
+		.DB 0b1111, 0b1110
+		.DB 0b1100, 0b1101
+		.DB 0b1000, 0b1001
+		.DB 0b1011, 0b1010
 
 start:
 	ldi	TEMP, 0xFF
@@ -33,12 +44,19 @@ main:
 	in VARS1, PINB
 	in VARS2, PINC
 	
+	andi VARS1, 0x0F	; use 4 bit
+
 	andi VARS2, 0x01
 	cpi  VARS2, 0x01
-	brne display_led
-	andi VARS1, 0x0F
-	ldi ZL, low(TABLE*2)
-	ldi ZH, high(TABLE*2)
+	brne convert_gray_to_binary	; high: binary->gray, low: gray->binary
+convert_bin_to_gray:
+	ldi ZL, low(BIN_TO_GRAY_TABLE*2)
+	ldi ZH, high(BIN_TO_GRAY_TABLE*2)
+	rjmp lookup
+convert_gray_to_binary:
+	ldi ZL, low(GRAY_TO_BIN_TABLE*2)
+	ldi ZH, high(GRAY_TO_BIN_TABLE*2)
+lookup:
 	add ZL, VARS1
 	ldi TEMP, 0
 	adc ZH, TEMP
